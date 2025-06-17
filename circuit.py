89647678bd7bfc03d03a11083d427f6263c4a7f3
Gabfuwak <gabin.maury0@gmail.com>
2025-06-17 12:13:48 +0200

    refac: factorize circuit drawing logic

--- circuit.py
+++ circuit.py
@@ -19,199 +19,144 @@ class Circuit:
         self.sections = sections # Liste de sections
 
     def draw(self):
+        self._draw_circuit_outlines()
+        self._draw_rails(is_inside_rail=True)
+        self._draw_rails(is_inside_rail=False)
+
+    def _draw_circuit_outlines(self):
         curr_pos = raylib.Vector2(400, 300) # TODO: remove hardcoded value
-        facing = raylib.Vector2(1,0) # Left: (-1, 0); Right: (1, 0), Up: (0, 1), Down: (0, -1) 
+        facing = raylib.Vector2(1,0) # Left: (-1, 0); Right: (1, 0), Up: (0, 1), Down: (0, -1)
         for section in self.sections:
             if section == SectionType.LONG:
-                section_offset = raylib.vector2_scale(facing, LONG_SECTION_LENGTH)
-                end_pos = raylib.vector2_add(curr_pos, section_offset)
-
-                # left side 
-                dir_to_left = raylib.vector2_rotate(facing, math.radians(-90))
-                offset_to_left = raylib.vector2_scale(dir_to_left, CIRCUIT_WIDTH/2)
-                left_side_start = raylib.vector2_add(curr_pos, offset_to_left)
-                left_side_end = raylib.vector2_add(end_pos, offset_to_left)
-                raylib.draw_line_v(left_side_start, left_side_end, raylib.DARKGREEN)
-
-                # right side
-                dir_to_right = raylib.vector2_rotate(facing, math.radians(90))
-                offset_to_right = raylib.vector2_scale(dir_to_right, CIRCUIT_WIDTH/2)
-                right_side_start = raylib.vector2_add(curr_pos, offset_to_right)
-                right_side_end = raylib.vector2_add(end_pos, offset_to_right)
-                raylib.draw_line_v(right_side_start, right_side_end, raylib.DARKGREEN)
-
-                # left rail 
-                dir_to_left = raylib.vector2_rotate(facing, math.radians(-90))
-                offset_to_left = raylib.vector2_scale(dir_to_left, CIRCUIT_WIDTH/4)
-                left_side_start = raylib.vector2_add(curr_pos, offset_to_left)
-                left_side_end = raylib.vector2_add(end_pos, offset_to_left)
-                raylib.draw_line_v(left_side_start, left_side_end, raylib.BLACK)
-
-                # right rail
-                dir_to_right = raylib.vector2_rotate(facing, math.radians(90))
-                offset_to_right = raylib.vector2_scale(dir_to_right, CIRCUIT_WIDTH/4)
-                right_side_start = raylib.vector2_add(curr_pos, offset_to_right)
-                right_side_end = raylib.vector2_add(end_pos, offset_to_right)
-                raylib.draw_line_v(right_side_start, right_side_end, raylib.BLACK)
-
-                curr_pos = end_pos
-
+                curr_pos, facing = self._draw_straight_outline(curr_pos, facing, LONG_SECTION_LENGTH, raylib.DARKGREEN)
             elif section == SectionType.SHORT:
-                section_offset = raylib.vector2_scale(facing, SHORT_SECTION_LENGTH)
-                end_pos = raylib.vector2_add(curr_pos, section_offset)
-
-                # left side 
-                dir_to_left = raylib.vector2_rotate(facing, math.radians(-90))
-                offset_to_left = raylib.vector2_scale(dir_to_left, CIRCUIT_WIDTH/2)
-                left_side_start = raylib.vector2_add(curr_pos, offset_to_left)
-                left_side_end = raylib.vector2_add(end_pos, offset_to_left)
-                raylib.draw_line_v(left_side_start, left_side_end, raylib.GREEN)
-
-                # right side
-                dir_to_right = raylib.vector2_rotate(facing, math.radians(90))
-                offset_to_right = raylib.vector2_scale(dir_to_right, CIRCUIT_WIDTH/2)
-                right_side_start = raylib.vector2_add(curr_pos, offset_to_right)
-                right_side_end = raylib.vector2_add(end_pos, offset_to_right)
-                raylib.draw_line_v(right_side_start, right_side_end, raylib.GREEN)
-
-                # left rail 
-                dir_to_left = raylib.vector2_rotate(facing, math.radians(-90))
-                offset_to_left = raylib.vector2_scale(dir_to_left, CIRCUIT_WIDTH/4)
-                left_side_start = raylib.vector2_add(curr_pos, offset_to_left)
-                left_side_end = raylib.vector2_add(end_pos, offset_to_left)
-                raylib.draw_line_v(left_side_start, left_side_end, raylib.BLACK)
-
-                # right rail
-                dir_to_right = raylib.vector2_rotate(facing, math.radians(90))
-                offset_to_right = raylib.vector2_scale(dir_to_right, CIRCUIT_WIDTH/4)
-                right_side_start = raylib.vector2_add(curr_pos, offset_to_right)
-                right_side_end = raylib.vector2_add(end_pos, offset_to_right)
-                raylib.draw_line_v(right_side_start, right_side_end, raylib.BLACK)
-
-                curr_pos = end_pos
-
+                curr_pos, facing = self._draw_straight_outline(curr_pos, facing, SHORT_SECTION_LENGTH, raylib.GREEN)
             elif section == SectionType.TURN_LEFT:
-                # On tourne d'abord facing pour pouvoir placer le centre du cercle et calculer l'angle de fin
-                new_facing = raylib.vector2_rotate(facing, math.radians(-90))
-
-                # On calcule le centre de l'arc de cercle
-                center_offset = raylib.vector2_scale(new_facing, TURN_RADIUS)
-                center = raylib.vector2_add(curr_pos, center_offset)
-
-                # On calcule la position de fin
-                end_offset = raylib.vector2_scale(facing, TURN_RADIUS)
-                curr_pos = raylib.vector2_add(center, end_offset)
-
-                start_angle = math.degrees(math.atan2(facing.y, facing.x))
-                end_angle = start_angle + 90
-                
-                # Note: En soit on pourrait draw un seul ring avec SHORT_SECTION_LENGTH inside et 2*SHORT_SECTION_LENGTH outside mais j'ai envie d'avoir que les outlines
-                # Interieur
-                raylib.draw_ring(
-                    center,
-                    SHORT_SECTION_LENGTH-1,
-                    SHORT_SECTION_LENGTH,
-                    start_angle,
-                    end_angle,
-                    32,
-                    raylib.RED
-                )
-
-                # Exterieur
-                raylib.draw_ring(
-                    center,
-                    2*SHORT_SECTION_LENGTH - 1,
-                    2*SHORT_SECTION_LENGTH,
-                    start_angle,
-                    end_angle,
-                    32,
-                    raylib.RED
-                )
-
-
-                # Rail exterieur
-                raylib.draw_ring(
-                    center,
-                    (2*SHORT_SECTION_LENGTH - SHORT_SECTION_LENGTH/4) - 1,
-                    (2*SHORT_SECTION_LENGTH - SHORT_SECTION_LENGTH/4),
-                    start_angle,
-                    end_angle,
-                    32,
-                    raylib.BLACK
-                )
-
-                # Rail interieur 
-                raylib.draw_ring(
-                    center,
-                    (SHORT_SECTION_LENGTH + SHORT_SECTION_LENGTH/4) - 1,
-                    (SHORT_SECTION_LENGTH + SHORT_SECTION_LENGTH/4),
-                    start_angle,
-                    end_angle,
-                    32,
-                    raylib.BLACK
-                )
-
-                facing = new_facing
-
-
+                curr_pos, facing = self._draw_turn_outline(curr_pos, facing, turn_right=False)
             elif section == SectionType.TURN_RIGHT:
-                # On tourne d'abord facing pour pouvoir placer le centre du cercle et calculer l'angle de fin
-                new_facing = raylib.vector2_rotate(facing, math.radians(90))
+                curr_pos, facing = self._draw_turn_outline(curr_pos, facing, turn_right=True)
 
-                # On calcule le centre de l'arc de cercle
-                center_offset = raylib.vector2_scale(new_facing, TURN_RADIUS)
-                center = raylib.vector2_add(curr_pos, center_offset)
+    def _draw_rails(self, is_inside_rail):
+        curr_pos = raylib.Vector2(400, 300) # TODO: remove hardcoded value
+        facing = raylib.Vector2(1,0) # Left: (-1, 0); Right: (1, 0), Up: (0, 1), Down: (0, -1)
+        for section in self.sections:
+            if section == SectionType.LONG:
+                curr_pos, facing = self._draw_straight_rail(curr_pos, facing, LONG_SECTION_LENGTH, is_inside_rail)
+            elif section == SectionType.SHORT:
+                curr_pos, facing = self._draw_straight_rail(curr_pos, facing, SHORT_SECTION_LENGTH, is_inside_rail)
+            elif section == SectionType.TURN_LEFT:
+                curr_pos, facing = self._draw_turn_rail(curr_pos, facing, turn_right=False, is_inside_rail=is_inside_rail)
+            elif section == SectionType.TURN_RIGHT:
+                curr_pos, facing = self._draw_turn_rail(curr_pos, facing, turn_right=True, is_inside_rail=is_inside_rail)
 
-                # On calcule la position de fin
-                end_offset = raylib.vector2_scale(facing, TURN_RADIUS)
-                curr_pos = raylib.vector2_add(center, end_offset)
+    def _draw_straight_outline(self, curr_pos, facing, length, color):
+        section_offset = raylib.vector2_scale(facing, length)
+        end_pos = raylib.vector2_add(curr_pos, section_offset)
 
-                start_angle = math.degrees(math.atan2(facing.y, facing.x))
-                end_angle = start_angle - 90
-                
-                # Interieur
-                raylib.draw_ring(
-                    center,
-                    SHORT_SECTION_LENGTH-1,
-                    SHORT_SECTION_LENGTH,
-                    start_angle,
-                    end_angle,
-                    32,
-                    raylib.RED
-                )
+        # left side
+        dir_to_left = raylib.vector2_rotate(facing, math.radians(-90))
+        offset_to_left = raylib.vector2_scale(dir_to_left, CIRCUIT_WIDTH/2)
+        left_side_start = raylib.vector2_add(curr_pos, offset_to_left)
+        left_side_end = raylib.vector2_add(end_pos, offset_to_left)
+        raylib.draw_line_v(left_side_start, left_side_end, color)
 
-                # Exterieur
-                raylib.draw_ring(
-                    center,
-                    2*SHORT_SECTION_LENGTH - 1,
-                    2*SHORT_SECTION_LENGTH,
-                    start_angle,
-                    end_angle,
-                    32,
-                    raylib.RED
-                )
+        # right side
+        dir_to_right = raylib.vector2_rotate(facing, math.radians(90))
+        offset_to_right = raylib.vector2_scale(dir_to_right, CIRCUIT_WIDTH/2)
+        right_side_start = raylib.vector2_add(curr_pos, offset_to_right)
+        right_side_end = raylib.vector2_add(end_pos, offset_to_right)
+        raylib.draw_line_v(right_side_start, right_side_end, color)
 
-                # Rail exterieur
-                raylib.draw_ring(
-                    center,
-                    (2*SHORT_SECTION_LENGTH - SHORT_SECTION_LENGTH/4) - 1,
-                    (2*SHORT_SECTION_LENGTH - SHORT_SECTION_LENGTH/4),
-                    start_angle,
-                    end_angle,
-                    32,
-                    raylib.BLACK
-                )
+        return end_pos, facing
 
-                # Rail interieur 
-                raylib.draw_ring(
-                    center,
-                    (SHORT_SECTION_LENGTH + SHORT_SECTION_LENGTH/4) - 1,
-                    (SHORT_SECTION_LENGTH + SHORT_SECTION_LENGTH/4),
-                    start_angle,
-                    end_angle,
-                    32,
-                    raylib.BLACK
-                )
+    def _draw_turn_outline(self, curr_pos, facing, turn_right):
+        # On tourne d'abord facing pour pouvoir placer le centre du cercle et calculer l'angle de fin
+        turn_angle = 90 if turn_right else -90
+        new_facing = raylib.vector2_rotate(facing, math.radians(turn_angle))
 
-                facing = new_facing
+        # On calcule le centre de l'arc de cercle
+        center_offset = raylib.vector2_scale(new_facing, TURN_RADIUS)
+        center = raylib.vector2_add(curr_pos, center_offset)
 
+        # On calcule la position de fin
+        end_offset = raylib.vector2_scale(facing, TURN_RADIUS)
+        curr_pos = raylib.vector2_add(center, end_offset)
+
+        start_angle = math.degrees(math.atan2(facing.y, facing.x))
+        end_angle = start_angle - turn_angle
+
+        # Note: En soit on pourrait draw un seul ring avec SHORT_SECTION_LENGTH inside et 2*SHORT_SECTION_LENGTH outside mais j'ai envie d'avoir que les outlines
+        # Interieur
+        raylib.draw_ring(
+            center,
+            SHORT_SECTION_LENGTH-1,
+            SHORT_SECTION_LENGTH,
+            start_angle,
+            end_angle,
+            32,
+            raylib.RED
+        )
+
+        # Exterieur
+        raylib.draw_ring(
+            center,
+            2*SHORT_SECTION_LENGTH - 1,
+            2*SHORT_SECTION_LENGTH,
+            start_angle,
+            end_angle,
+            32,
+            raylib.RED
+        )
+
+        return curr_pos, new_facing
+
+    def _draw_straight_rail(self, curr_pos, facing, length, is_inside_rail):
+        section_offset = raylib.vector2_scale(facing, length)
+        end_pos = raylib.vector2_add(curr_pos, section_offset)
+
+        # left rail si inside, right rail si outside
+        angle = -90 if is_inside_rail else 90
+        dir_to_side = raylib.vector2_rotate(facing, math.radians(angle))
+        offset_to_side = raylib.vector2_scale(dir_to_side, CIRCUIT_WIDTH/4)
+        rail_start = raylib.vector2_add(curr_pos, offset_to_side)
+        rail_end = raylib.vector2_add(end_pos, offset_to_side)
+        raylib.draw_line_v(rail_start, rail_end, raylib.BLACK)
+
+        return end_pos, facing
+
+    def _draw_turn_rail(self, curr_pos, facing, turn_right, is_inside_rail):
+        # On tourne d'abord facing pour pouvoir placer le centre du cercle et calculer l'angle de fin
+        turn_angle = 90 if turn_right else -90
+        new_facing = raylib.vector2_rotate(facing, math.radians(turn_angle))
+
+        # On calcule le centre de l'arc de cercle
+        center_offset = raylib.vector2_scale(new_facing, TURN_RADIUS)
+        center = raylib.vector2_add(curr_pos, center_offset)
+
+        # On calcule la position de fin
+        end_offset = raylib.vector2_scale(facing, TURN_RADIUS)
+        curr_pos = raylib.vector2_add(center, end_offset)
+
+        start_angle = math.degrees(math.atan2(facing.y, facing.x))
+        end_angle = start_angle - turn_angle
+
+        # Rail interieur si inside, exterieur si outside
+        rail_radius = (SHORT_SECTION_LENGTH + SHORT_SECTION_LENGTH/4) if is_inside_rail else (2*SHORT_SECTION_LENGTH - SHORT_SECTION_LENGTH/4)
+        raylib.draw_ring(
+            center,
+            rail_radius - 1,
+            rail_radius,
+            start_angle,
+            end_angle,
+            32,
+            raylib.BLACK
+        )
+
+        return curr_pos, new_facing
+
+
+# Export direct des valeurs
+LONG = SectionType.LONG
+SHORT = SectionType.SHORT
+TURN_LEFT = SectionType.TURN_LEFT
+TURN_RIGHT = SectionType.TURN_RIGHT
